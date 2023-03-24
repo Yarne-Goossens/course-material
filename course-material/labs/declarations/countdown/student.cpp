dd5afaeb75abfff0448ad720f2f4dd8a8ce80d17
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-03-24 08:44:30 +0100

    eerste oefeningen

--- course-material/labs/declarations/countdown/student.cpp
+++ course-material/labs/declarations/countdown/student.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
 void countdown(int from)
 {
-    // TODO
+    for (int i = from; i != 0; --i)
+    {
+        std::cout << i << std::endl;
+    }
 }
--- course-material/labs/declarations/nth-prime/student.cpp
+++ course-material/labs/declarations/nth-prime/student.cpp
@@ -2,6 +2,22 @@
 // (no, 1 is not a prime)
 unsigned nth_prime(unsigned n)
 {
-    // TODO
-    throw "Not yet implemented";
+    int i, y;
+    unsigned primes = 0;
+    unsigned getal = 0;
+    bool is_prime = true;
+
+    while (primes < n) {
+        for (i = 2; i <= y / 2; ++i) {
+            if (y % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime == true) {
+            primes++;
+        }
+        getal++;
+    }
+    return getal;
 }
